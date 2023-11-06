a252966c8edfdcf90699f432a0dc190a613b0d85
Sarah Brown <quesarahsarah90@gmail.com>
2023-11-06 16:37:56 -0600

    week 3

--- week3_python/week3_query_with_python_api.py
+++ week3_python/week3_query_with_python_api.py
@@ -33,42 +33,63 @@ logger.LogManager.getLogger("org.apache.spark.SparkEnv"). setLevel( logger.Level
 
 #Question 1: Read the tab separated file named "resources/reviews.tsv.gz" into a dataframe.
 #You will use the "reviews" dataframe defined here to answer all the questions below...
+reviews = spark.read.csv("resources/reviews.tsv.gz", sep="\t", header=True)
 
 #Question 2: Display the schema of the dataframe.
+reviews.select('*').printSchema()
 
 #Question 3: How many records are in the dataframe? 
 #Store this number in a variable named "reviews_count".
+reviews_count = reviews.count()
+print(reviews_count) # 145431
 
 #Question 4: Print the first 5 rows of the dataframe. 
 #Some of the columns are long - print the entire record, regardless of length.
+reviews.show(n=5,truncate=False)
 
 #Question 5: Create a new dataframe based on "reviews" with exactly 1 column: the value of the product category field.
 #Look at the first 50 rows of that dataframe. 
 #Which value appears to be the most common?
+reviews_product_category = reviews.select('product_category')
+reviews_product_category.show(n=50)
 
 #Question 6: Find the most helpful review in the dataframe - the one with the highest number of helpful votes.
 #What is the product title for that review? How many helpful votes did it have?
+# ***** don't know how to cast as int here *************************************
+reviews.cast("Integer")
+reviews.orderBy(reviews['helpful_votes'].desc()).show(n=1)
 
 #Question 7: How many reviews have a 5 star rating?
+print(reviews.filter(reviews['star_rating']=='5').count()) #80677
+
 
 #Question 8: Currently every field in the data file is interpreted as a string, but there are 3 that should really be numbers.
 #Create a new dataframe with just those 3 columns, except cast them as "int"s.
 #Look at 10 rows from this dataframe.
 
+# *********** same as Q6, don't know how to cast here ***************************
+reviews.cast()
 #Question 8: Find the date with the most purchases.
 #Print the date and total count of the date with the most purchases
+
+
 
 #Question 9: Add a column to the dataframe named "review_timestamp", representing the current time on your computer. 
 #Hint: Check the documentation for a function that can help: https://spark.apache.org/docs/3.1.3/api/python/reference/pyspark.sql.html#functions
 #Print the schema and inspect a few rows of data to make sure the data is correctly populated.
 
+reviews_with_timestamp = reviews.withColumn("review_timestamp", current_timestamp())
+reviews_with_timestamp.show(n=5)
+
 #Question 10: Write the dataframe with load timestamp to s3a://hwe-$CLASS/$HANDLE/bronze/reviews_static in Parquet format.
 #Make sure to write it using overwrite mode: append will keep appending duplicates, which will cause problems in later labs...
+reviews_with_timestamp.write.mode("overwrite").parquet("s3a://hwe-fall-2023/sbrown/bronze/reviews_static")
 
 #Question 11: Read the tab separated file named "resources/customers.tsv.gz" into a dataframe
 #Write to S3 under s3a://hwe-$CLASS/$HANDLE/bronze/customers
 #Make sure to write it using overwrite mode: append will keep appending duplicates, which will cause problems in later labs...
 #There are no questions to answer about this data set right now, but you will use it in a later lab...
-
+customers = spark.read.csv("resources/customers.tsv.gz", sep="\t", header=True)
+customers.write.mode("overwrite").parquet("s3a://hwe-fall-2023/sbrown/bronze/customers")
 # Stop the SparkSession
 spark.stop()
